99f4c9a32a2fb367aa1c4687b497e147bea3d61e
coldSunset <ravindusiri@gmail.com>
2020-08-26 18:00:30 +1000

    ch12 start

---
+++ Ch12/longvalues.c
@@ -0,0 +1,42 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <unistd.h> 
+#include <errno.h> 
+#include <pthread.h> 
+
+void error(char* msg)
+{
+	fprintf(stderr, "%s: %s\n", msg, strerror(errno)); 
+	exit(1); 
+}
+
+void* do_stuff(void* param)
+{
+	long thread_no = (long)param; 
+	printf("Thread number %ld\n", thread_no); 
+	return (void*)(thread_no +1); 
+}
+
+int main(int argc, char* argv[])
+{
+	pthread_t threads[20]; 
+	long t; 
+
+	for(t=0; t<3; t++)
+	{
+		if(pthread_create(&threads[t], NULL, do_stuff, (void*)t) == -1) 
+			error("Error creating thread"); 
+	} 
+
+	void* result; 
+	for(t=0 ; t<3; t++)
+	{
+		if(pthread_join(threads[t], &result) == -1)
+			error("Error joining thread"); 
+		printf("Thread %ld returned %ld\n", t, (long)result);  
+	}
+
+
+	return 0; 
+}
